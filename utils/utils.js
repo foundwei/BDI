1b19252f8832390d3c65c789eb271ec06f4f867b
foundwei <foundwei@qq.com>
2017-07-03 08:37:17 +0000

    use log4js for log trace

--- utils/utils.js
+++ utils/utils.js
@@ -2,7 +2,26 @@
  * @fileoverview The utilities functions/methods go into this file.
  *
  * @author Wei Chengli (foundwei@qq.com)
- * @version 1.0 (2017-03-24)
+ * @version 1.0 (2017-07-03)
  */
- 
- 
+
+var log4js = require('log4js');
+log4js.configure({
+  appenders: [
+    { type: 'console' },
+    {
+      type: 'file', 
+      filename: 'logs/access.log', 
+      maxLogSize: 1024,
+      backups:4,
+      category: 'normal' 
+    }
+  ],
+  replaceConsole: true
+});
+
+exports.logger = function(name) {
+  var logger = log4js.getLogger(name);
+  logger.setLevel('INFO');
+  return logger;
+}
