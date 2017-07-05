8e6541f811d32cf1b99e0c60326114d4e71465aa
foundwei <foundwei@qq.com>
2017-07-05 00:47:50 +0000

    implement CRUD of models->mysql->Ships

--- models/mysql/Ships.js
+++ models/mysql/Ships.js
@@ -7,6 +7,7 @@
  
 var Sequelize = require('sequelize');
 var orm = require("../../utils/mysqlconn.js");
+var logger = require('../../utils/utils.js').logger('model-Ships');
  
 // OR mapping
 var Ships = orm.define('Ships', {
@@ -41,5 +42,40 @@ var ShipsDAO = function() {};
  * implement the methods in ShipsDAO.prototype.
  * 
  */
+ShipsDAO.prototype.save = function(obj) {
+  Ships.create(obj).on('success', function(msg) {
+    logger.info(msg);
+  }).on('failure', function(err){
+    logger.error(err);
+  });
+};
+
+ShipsDAO.prototype.update = function(shid, obj) {
+  Ships.update(obj, {shid : shid}).on('success', function(msg) {
+    logger.info(msg);
+  }).on('failure', function(err){
+    logger.error(err);
+  });
+};
+
+ShipsDAO.prototype.delete = function(shid) {
+  Ships.destroy({shid : shid}).on('success', function(msg) {
+    logger.info(msg);
+  }).on('failure', function(err){
+    logger.error(err);
+  });
+};
+
+/**
+ * pn : page number, start from 1 not 0
+ * ps : page size
+ */
+ShipsDAO.prototype.findAllByPage = function(pn, ps, callback) {
+  Ships.findAll({limit: ps, offset: (pn - 1) * ps}, {raw : true, logging : true, plain : false}).on('success', function(res) {
+    callback(res);
+  }).on('failure', function(err){
+    logger.error(err);
+  })
+};
  
 module.exports = new ShipsDAO();
